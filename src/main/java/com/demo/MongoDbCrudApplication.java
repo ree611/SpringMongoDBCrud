cbc0002ed4c54c5811c892f69e01d05c407daba2
ree611 <reenadhingra98@gmail.com>
2020-07-30 00:17:28 +0530

    My very first commit

---
+++ src/main/java/com/demo/MongoDbCrudApplication.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDbCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoDbCrudApplication.class, args);
+		System.out.println("start");
+	}
+
+}
